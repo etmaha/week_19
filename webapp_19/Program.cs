fb16e32254b0657db4ea68d5f346cf998eb6b8a1
etmaha <etmaha001@gmail.com>
2024-05-20 18:01:04 -0500

    Added support to webapi_19 for select, insert, update, and delete on the employee table

--- webapp_19/Program.cs
+++ webapp_19/Program.cs
@@ -13,8 +13,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
+
 
 app.UseRouting();
 
@@ -23,3 +25,5 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
